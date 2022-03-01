400b6de708c6da9afb3f76e6b9f6a45c9bfe11fd
diegolins10 <dede140126>
2022-03-01 20:44:28 -0300

    adicionando as rotas da aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
