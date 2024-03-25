cf153b8872a6573b155e613182e8cdbe465cf4f7
diegolins10 <dede140126>
2024-03-25 00:56:30 -0300

    91. Usando Animação com Keyframes nos Itens do Carrinho

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,12 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { ShoppingCartService } from './shopping-cart.service';
+import { animate, state, style, transition, trigger, keyframes } from '@angular/animations';
+
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.css']
+  styleUrls: ['./shopping-cart.component.css'],
+  animations: [
+    trigger('row', [
+      state('ready', style({ opacity: 1 })),
+      // essa transition animação causa o efeito de arrasta pra direita quando adiciona... o item
+      transition('void => ready', [
+        animate('300ms 0s ease-in', keyframes ([
+          style({opacity: 0, transform: 'translateX(-30px)', offset:0}),
+          style({opacity: 0.8, transform: 'translateX(10px)', offset:0.8}),
+          style({opacity: 1, transform: 'translateX(0px)', offset:1})
+        ]))
+      ]),
+      // essa transition animação causa o efeito de arrasta pra esqueda quando remove... o item
+      transition('ready => void', [
+        animate('300ms 0s ease-in', keyframes ([
+          style({opacity: 1, transform: 'translateX(0px)', offset:0}),
+          style({opacity: 0.8, transform: 'translateX(10px)', offset:0.2}),
+          style({opacity: 0, transform: 'translateX(-30px)', offset:1})
+        ]))
+      ])
+    ])
+  ] // essa animação causa o efeito de arrasta pra direita... o item
 })
 export class ShoppingCartComponent implements OnInit {
+
+  rowState = 'ready';
 
   constructor(private shoppingCartService: ShoppingCartService) { }
 
